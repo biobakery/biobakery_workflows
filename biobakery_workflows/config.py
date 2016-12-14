a335f769b0db5296b0b1b5e4a776bbebaa7de4c8
Lauren McIver <lauren.j.mciver@gmail.com>
2016-12-14 16:44:49 -0700

    Add initial config file

---
+++ biobakery_workflows/config.py
@@ -0,0 +1,27 @@
+"""
+bioBakery Workflows: config module
+Configuration settings for workflows and tasks
+
+Copyright (c) 2016 Harvard School of Public Health
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+kneaddata_db_human_genome = "/n/huttenhower_lab/data/kneaddata_databases/"
+kneaddata_db_human_metatranscriptome = "/n/huttenhower_lab/data/kneaddata_databases/transcriptome/"
