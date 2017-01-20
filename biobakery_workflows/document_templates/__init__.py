201a487ad4a09924de19fb91dc7e63fdb51b7481
Lauren McIver <lauren.j.mciver@gmail.com>
2017-01-20 14:10:46 -0700

    Update the find template function so it only matches on the template of a given name not a subset

--- biobakery_workflows/document_templates/__init__.py
+++ biobakery_workflows/document_templates/__init__.py
@@ -5,11 +5,12 @@ def get_template(name):
     """ Get the location of the template by name """
     
     # get all of the templates in this folder
+    template_extension=".template.py"
     template_install_folder=os.path.dirname(os.path.realpath(__file__))
-    templates=filter(lambda file: file.endswith("template.py"),os.listdir(template_install_folder))
+    templates=filter(lambda file: file.endswith(template_extension),os.listdir(template_install_folder))
 
     # return the template with the name
-    found_template=list(filter(lambda file: file.startswith(name), templates))
+    found_template=list(filter(lambda file: file.startswith(name+template_extension), templates))
 
     if found_template:
         found_template=os.path.join(template_install_folder,found_template[0])
