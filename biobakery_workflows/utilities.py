8481caed0fcb54e94ceae2e747bdc1cc50bf28aa
Lauren McIver <lauren.j.mciver@gmail.com>
2017-02-27 15:28:44 -0700

    Add error messages to match_files function to capture cases where a match can not be found in either folder

--- biobakery_workflows/utilities.py
+++ biobakery_workflows/utilities.py
@@ -265,6 +265,10 @@ def match_files(files1,files2,mapping):
             # check for the pair
             pair1.append(file1[0])
             pair2.append(file2[0])
+        elif len(file1) == 0:
+            print("Warning: Unable to find file with key, " + item1 + " in folder " + os.path.dirname(files1))
+        elif len(file2) == 0:
+            print("Warning: Unable to find file with key, " + item2 + " in folder " + os.path.dirname(files2))
         else:
             print("Warning: Duplicate files found for mapping keys: " + item1 + " " + item2)
 
