cd079edee06a645a6464b40613babadd35b0a726
Lauren McIver <lauren.j.mciver@gmail.com>
2017-03-09 17:47:07 -0700

    Add initial 16s_vis workflow

---
+++ biobakery_workflows/document_templates/16S.template.py
@@ -0,0 +1,75 @@
+
+#+ echo=False
+import numpy
+
+from biobakery_workflows import utilities
+
+from anadama2 import PweaveDocument
+
+document=PweaveDocument()  
+
+# get the variables for this document generation task
+vars = document.get_vars()
+
+#' # Taxonomy
+
+#+ echo=False
+
+# read in the otu table data
+samples, ids, taxonomy, data = utilities.read_otu_table(vars["otu_table"])
+
+# create a plot of total counts from the otu table
+# get the total number of counts for each sample
+counts={sample:sum(column) for sample,column in zip(samples,numpy.transpose(data))}
+# order the counts to put the samples with the largest counts first in the plot
+sorted_samples=sorted(counts,key=counts.get, reverse=True)
+sorted_counts=[counts[sample] for sample in sorted_samples]
+
+document.plot_barchart(sorted_counts, sorted_samples, title="OTU Counts by Sample",
+    ylabel="Counts", xlabel="Samples")
+
+#+ echo=False
+
+# plot the top taxa by genus level, plotting the relative abundance values
+max_taxa=15
+# get the relative abundance values for the samples
+relab_data = utilities.relative_abundance(data)
+# get the taxa summarized by genus level
+genus_level_taxa, genus_level_data = utilities.taxa_by_level(taxonomy, relab_data, level=5)
+# get the top rows of the relative abundance data
+top_taxa, top_data = utilities.top_rows(genus_level_taxa, genus_level_data, max_taxa, function="average")
+# shorten the top taxa names to just the genus level for plotting
+top_taxa_short_names = utilities.taxa_shorten_name(top_taxa, level=5, remove_identifier=True)
+
+document.plot_stacked_barchart(top_data, row_labels=top_taxa_short_names, 
+    column_labels=samples, title="Top "+str(max_taxa)+" genera by average abundance",
+    ylabel="Relative abundance", legend_title="Genera")
+
+#+ echo=False
+
+# plot the relative abundance of the top terminal taxa
+# get the terminal taxa
+terminal_taxa_relab, terminal_data_relab = utilities.terminal_taxa(taxonomy, relab_data)
+# get the top rows of terminal taxa
+top_terminal_taxa, top_terminal_data = utilities.top_rows(terminal_taxa_relab, terminal_data_relab, max_taxa, function="average")
+
+# reduce the taxa names to just the most specific identifier
+shorted_names=[taxon.split(";")[-1] for taxon in top_terminal_taxa]
+
+document.plot_stacked_barchart(top_terminal_data, row_labels=shorted_names, 
+    column_labels=samples, title="Top "+str(max_taxa)+" terminal taxa by average abundance",
+    ylabel="Relative abundance", legend_title="Terminal taxa")
+
+#+ echo=False
+
+# plot the top terminal node taxa in a PCOA
+# provide data as values [0-1] organized as samples as columns and features as rows
+# filter out any zero rows
+taxa_nonzero, data_nonzero = utilities.filter_zero_rows(terminal_taxa_relab, terminal_data_relab)
+
+document.show_pcoa(samples, taxa_nonzero, data_nonzero, title="Ordination of terminal taxa")
+
+
+
+
+
---
+++ biobakery_workflows/workflows/16s_vis.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+"""
+bioBakery Workflows: 16S workflow for visualization
+
+Copyright (c) 2017 Harvard School of Public Health
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import os
+
+# import the workflow class from anadama2
+from anadama2 import Workflow
+
+# import the document templates from biobakery_workflows
+from biobakery_workflows import document_templates
+
+# create a workflow instance, providing the version number and description
+# remove the input folder option as it will be replaced with multiple input files
+workflow = Workflow(version="0.1", remove_options=["input"],
+                    description="A workflow for 16S visualization")
+
+# add the custom arguments to the workflow
+workflow.add_argument("otu-table",desc="the closed reference taxonomic profile",required=True)
+workflow.add_argument("project-name",desc="the name of the project")
+workflow.add_argument("introduction-text",desc="the text to include in the intro of the report",
+    default="The data was run through the standard workflow for 16S sequencing.")
+
+# get the arguments from the command line
+args = workflow.parse_args()
+
+# add the document to the workflow
+workflow.add_document(
+    templates=[document_templates.get_template("header"),
+               document_templates.get_template("16S")],
+    depends=[args.otu_table], 
+    targets=workflow.name_output_files("16S_report.pdf"),
+    vars={"title":"16S Report",
+          "project":args.project_name,
+          "introduction_text":args.introduction_text,
+          "otu_table":os.path.abspath(args.otu_table)})
+
+# start the workflow
+workflow.go()
