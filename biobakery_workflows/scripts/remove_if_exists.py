32c525fd955f6909a700955146464176883325e7
Lauren McIver <lauren.j.mciver@gmail.com>
2017-03-16 10:34:26 -0600

    Add script to remove file if it exists, useful to run before a task in a workflow that will not overwrite an output file (script from bakefiles 16s workflows/repository)

---
+++ biobakery_workflows/scripts/remove_if_exists.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+""" This script will remove a file if it exists.
+    It will report error messages from trying to remove the file. """
+    
+import sys
+
+try:
+    import argparse
+except ImportError:
+    sys.exit("Please upgrade to at least python v2.7")
+    
+import os
+
+def parse_arguments(args):
+    """ Parse the arguments from the user"""
+    
+    parser=argparse.ArgumentParser(description="Remove file if it exists.")
+    parser.add_argument('file',help="File to remove")
+
+    return parser.parse_args()    
+
+    
+def main():
+    # parse arguments
+    args = parse_arguments(sys.argv)
+    
+    # check if the file exists
+    if os.path.isfile(args.file):
+        # if it exists, then try to remove
+        # Report error messages if unable to remove file that exists
+        try:
+            os.unlink(args.file)
+        except EnvironmentError:
+            sys.exit("ERROR: Unable to remove file: " + args.file)
+    
+    
+if __name__ == "__main__":
+    main()
