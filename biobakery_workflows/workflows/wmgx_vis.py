72221c2f216df8e5e5b9e5393858c03635d71983
Lauren McIver <lauren.j.mciver@gmail.com>
2017-05-19 12:18:23 -0600

    Update wmgx_vis command line options to a single input folder replacing the individual input options

--- biobakery_workflows/workflows/wmgx_vis.py
+++ biobakery_workflows/workflows/wmgx_vis.py
@@ -31,30 +31,46 @@ from anadama2 import Workflow
 from biobakery_workflows import document_templates
 from biobakery_workflows import utilities
 
+# import the files for descriptions and paths
+from biobakery_workflows import files
+
 # create a workflow instance, providing the version number and description
 # remove the input folder option as it will be replaced with multiple input files
 workflow = Workflow(version="0.1", remove_options=["input"],
                     description="A workflow for whole metagenome shotgun sequence visualization")
 
+# list the required and optional files for the workflow
+# these are expected to be included in the input folder
+input_files={"required":["kneaddata_read_counts","taxonomic_profile","pathabundance","humann2_read_counts","feature_counts"]}
+
+# create a custom description for the input argument listing all expected input files
+input_desc="A folder containing the final products from the wmgx data workflow.\n\nThe input folder should include the following:\n\n"
+input_desc+=files.ShotGun.list_file_path_description("",input_files)
+
+# add the custom arguments to the workflow                          
+workflow.add_argument("input",desc=input_desc,required=True)
+
 # add the custom arguments to the workflow
-workflow.add_argument("qc-counts",desc="the kneaddata read counts table",required=True)
-workflow.add_argument("taxonomic-profile",desc="the merged taxonomic profiles",required=True)
-workflow.add_argument("pathabundance",desc="the pathway abundances",required=True)
-workflow.add_argument("read-counts", desc="the read counts from the humann2 log files",required=True)
-workflow.add_argument("feature-counts", desc="the counts of features (gene families, ecs, and pathways)", required=True)
 workflow.add_argument("project-name",desc="the name of the project")
-workflow.add_argument("introduction-text",desc="the text to include in the intro of the report [default: %default]",
+workflow.add_argument("introduction-text",desc="the text to include in the intro of the report",
     default="The data was run through the standard workflow for whole metagenome shotgun sequencing.")
-workflow.add_argument("format",desc="the format for the report, pdf or html [default: %default]", default="pdf")
-workflow.add_argument("contaminate-database",desc="the database used for contaminate read filtering for quality control [default: %default]", default="hg38")
+workflow.add_argument("format",desc="the format for the report, pdf or html", default="pdf")
+workflow.add_argument("contaminate-database",desc="the database used for contaminate read filtering for quality control", default="hg38")
 
 # get the arguments from the command line
 args = workflow.parse_args()
 
+# get the paths for the required files and check they are found
+qc_counts=files.ShotGun.path("kneaddata_read_counts",args.input, error_if_not_found=True)
+taxonomic_profile=files.ShotGun.path("taxonomic_profile",args.input, error_if_not_found=True)
+pathabundance=files.ShotGun.path("pathabundance",args.input, error_if_not_found=True)
+read_counts=files.ShotGun.path("humann2_read_counts",args.input, error_if_not_found=True)
+feature_counts=files.ShotGun.path("feature_counts",args.input, error_if_not_found=True)
+
 # select the templates based on the qc data
 templates=[document_templates.get_template("header")]
 
-if utilities.is_paired_table(args.qc_counts):
+if utilities.is_paired_table(qc_counts):
     templates+=[document_templates.get_template("quality_control_paired_dna")]
 else:
     templates+=[document_templates.get_template("quality_control_single_dna")]
@@ -65,17 +81,17 @@ templates+=[document_templates.get_template("taxonomy"),
 # add the document to the workflow
 doc_task=workflow.add_document(
     templates=templates,
-    depends=[args.qc_counts, args.taxonomic_profile, args.pathabundance,
-             args.read_counts, args.feature_counts], 
+    depends=[qc_counts, taxonomic_profile, pathabundance,
+             read_counts, feature_counts], 
     targets=workflow.name_output_files("wmgx_report."+args.format),
     vars={"title":"Metagenome Report",
           "project":args.project_name,
           "introduction_text":args.introduction_text,
-          "dna_read_counts":args.qc_counts,
-          "taxonomic_profile":args.taxonomic_profile,
-          "dna_pathabundance":args.pathabundance,
-          "read_counts":args.read_counts,
-          "feature_counts":args.feature_counts,
+          "dna_read_counts":qc_counts,
+          "taxonomic_profile":taxonomic_profile,
+          "dna_pathabundance":pathabundance,
+          "read_counts":read_counts,
+          "feature_counts":feature_counts,
           "format":args.format,
           "contaminate_database":args.contaminate_database})
 
