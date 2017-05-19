ac0839a3770406565b678adcda4e3605f2860c36
Lauren McIver <lauren.j.mciver@gmail.com>
2017-05-19 12:17:35 -0600

    Update wmgx_wmtx_vis workflow command line options to a single input folder replacing the eight input files

--- biobakery_workflows/workflows/wmgx_wmtx_vis.py
+++ biobakery_workflows/workflows/wmgx_wmtx_vis.py
@@ -24,33 +24,53 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+import os
+
 # import the workflow class from anadama2
 from anadama2 import Workflow
 
 # import the document templates from biobakery_workflows
 from biobakery_workflows import document_templates
 
+# import the files for descriptions and paths
+from biobakery_workflows import files
+
 # create a workflow instance, providing the version number and description
 # remove the input folder option as it will be replaced with multiple input files
 workflow = Workflow(version="0.1", remove_options=["input"],
                     description="A workflow for whole metagenome and metatranscriptome shotgun sequence visualization")
 
-# add the custom arguments to the workflow
-workflow.add_argument("wmgx-qc-counts",desc="the kneaddata read counts table for wmgx samples",required=True)
-workflow.add_argument("wmtx-qc-counts",desc="the kneaddata read counts table for wmtx samples",required=True)
-workflow.add_argument("taxonomic-profile",desc="the merged taxonomic profiles for the wmgx samples",required=True)
-workflow.add_argument("wmgx-pathabundance",desc="the pathway abundances for the wmgx samples",required=True)
-workflow.add_argument("wmgx-read-counts", desc="the read counts from the humann2 log files for wmgx samples")
-workflow.add_argument("wmtx-read-counts", desc="the read counts from the humann2 log files for wmtx samples")
-workflow.add_argument("wmgx-feature-counts", desc="the counts of features (gene families, ecs, and pathways) for wmgx samples")
-workflow.add_argument("wmtx-feature-counts", desc="the counts of features (gene families, ecs, and pathways) for wmtx samples")
+# list the required and optional files for the workflow
+# these are expected to be included in the input folder
+wmgx_input_files={"required":["kneaddata_read_counts","taxonomic_profile","pathabundance"],"optional":["humann2_read_counts","feature_counts"]}
+wmtx_input_files={"required":["kneaddata_read_counts"],"optional":["humann2_read_counts","feature_counts"]}
+
+# create a custom description for the input argument listing all expected input files
+input_desc="A folder containing the final products from the wmgx_mwtx data workflow.\n\nThe input folder should include the following:\n\n"
+input_desc+="Whole Metagenome Shotgun\n---------------------------------\n"
+input_desc+=files.ShotGun.list_file_path_description(files.ShotGun.wmgx_folder_name,wmgx_input_files)
+input_desc+="\n\nWhole Metatranscriptome Shotgun\n---------------------------------\n"
+input_desc+=files.ShotGun.list_file_path_description(files.ShotGun.wmtx_folder_name,wmtx_input_files)
+
+# add the custom arguments to the workflow                          
+workflow.add_argument("input",desc=input_desc,required=True)
 workflow.add_argument("project-name",desc="the name of the project")
-workflow.add_argument("introduction-text",desc="the text to include in the intro of the report [default: %default]",
+workflow.add_argument("introduction-text",desc="the text to include in the intro of the report",
     default="The data was run through the standard workflow for whole metagenome and metatranscriptome shotgun sequencing.")
-workflow.add_argument("format",desc="the format for the report, pdf or html [default: %default]", default="pdf")
+workflow.add_argument("format",desc="the format for the report, pdf or html", default="pdf")
 
 # get the arguments from the command line
 args = workflow.parse_args()
+
+# set the input folders for the wmgx and wmtx files
+wmgx_input_folder=os.path.join(args.input,files.ShotGun.wmgx_folder_name)
+wmtx_input_folder=os.path.join(args.input,files.ShotGun.wmtx_folder_name)
+
+# get the paths for the required files and check they are found
+wmgx_qc_counts=files.ShotGun.path("kneaddata_read_counts",wmgx_input_folder, error_if_not_found=True)
+wmtx_qc_counts=files.ShotGun.path("kneaddata_read_counts",wmtx_input_folder, error_if_not_found=True)
+taxonomic_profile=files.ShotGun.path("taxonomic_profile",wmgx_input_folder, error_if_not_found=True)
+pathabundance=files.ShotGun.path("pathabundance",wmgx_input_folder, error_if_not_found=True)
 
 # add the document to the workflow
 doc_task=workflow.add_document(
@@ -58,20 +78,20 @@ doc_task=workflow.add_document(
                document_templates.get_template("quality_control_paired_dna_rna"),
                document_templates.get_template("taxonomy"),
                document_templates.get_template("functional_dna_rna")],
-    depends=[args.wmgx_qc_counts, args.wmtx_qc_counts,
-             args.taxonomic_profile, args.wmgx_pathabundance], 
+    depends=[wmgx_qc_counts, wmtx_qc_counts,
+             taxonomic_profile, pathabundance], 
     targets=workflow.name_output_files("wmgx_wmtx_report."+args.format),
     vars={"title":"Metagenome and Metatranscriptome Report",
           "project":args.project_name,
           "introduction_text":args.introduction_text,
-          "dna_read_counts":args.wmgx_qc_counts,
-          "rna_read_counts":args.wmtx_qc_counts,
-          "dna_aligned_read_counts":args.wmgx_read_counts,
-          "rna_aligned_read_counts":args.wmtx_read_counts,
-          "dna_feature_counts":args.wmgx_feature_counts,
-          "rna_feature_counts":args.wmtx_feature_counts,
-          "taxonomic_profile":args.taxonomic_profile,
-          "dna_pathabundance":args.wmgx_pathabundance,
+          "dna_read_counts":wmgx_qc_counts,
+          "rna_read_counts":wmtx_qc_counts,
+          "dna_aligned_read_counts":files.ShotGun.path("humann2_read_counts",wmgx_input_folder, none_if_not_found=True),
+          "rna_aligned_read_counts":files.ShotGun.path("humann2_read_counts",wmtx_input_folder, none_if_not_found=True),
+          "dna_feature_counts":files.ShotGun.path("feature_counts",wmgx_input_folder, none_if_not_found=True),
+          "rna_feature_counts":files.ShotGun.path("feature_counts",wmtx_input_folder, none_if_not_found=True),
+          "taxonomic_profile":taxonomic_profile,
+          "dna_pathabundance":pathabundance,
           "format":args.format})
 
 # add an archive of the document and figures, removing the log file
