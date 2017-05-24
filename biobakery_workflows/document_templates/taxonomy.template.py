41327add910566a74ed6fc75b0d06b06b4de1a05
Lauren McIver <lauren.j.mciver@gmail.com>
2017-05-24 16:05:48 -0600

    Update the taxonomy template to use the visualizations functions and files attributes

--- biobakery_workflows/document_templates/taxonomy.template.py
+++ biobakery_workflows/document_templates/taxonomy.template.py
@@ -7,9 +7,8 @@ min_abundance=0.01
 min_samples=10
 max_sets_heatmap=25
 max_sets_barplot=15
-max_table_rows=20
 
-from biobakery_workflows import utilities
+from biobakery_workflows import utilities, visualizations, files
 
 from anadama2 import PweaveDocument
 
@@ -69,23 +68,14 @@ species_counts_after_filter=count_filtered_columns(filtered_species_data, min=0)
 all_species_counts=[[a,b] for a,b in zip(species_counts, species_counts_after_filter)]
 
 # create a table of the data in the output folder
-species_table_file = os.path.join(document.data_folder,"species_counts_table.tsv")
-document.write_table(["# Sample","Total","After filter"],samples, all_species_counts, species_table_file)
+document.write_table(["# Sample","Total","After filter"],samples, all_species_counts,
+    files.ShotGunVis.path("species_counts",document.data_folder))
 
-# if there are not many samples, then show the full table
-if len(samples) <= max_table_rows:
-    document.show_table(all_species_counts,samples,["Total","After filter"],
-        title="Total species per sample")
-    message="A data file exists of this table: "
-else:
-    # reduce the table shown to only the first few samples
-    document.show_table(all_species_counts[:max_table_rows],samples[:max_table_rows],["Total","After filter"],
-        title="Total species per sample (partial table)")    
-    message="The table is too large to include the full table in this document."+\
-        " A partial table is shown which includes only "+str(max_table_rows)+" samples."+\
-        " Please see the data file for the full table: "
-#' <% print(message) %>
-#' [species_counts_table.tsv](data/species_counts_table.tsv)
+# show the table, reducing the rows if there are lots of samples
+table_message=visualizations.show_table_max_rows(document, all_species_counts, samples,
+    ["Total","After filter"],"Total species per sample",files.ShotGunVis.path("species_counts"))
+
+#' <%= table_message %>
 
 #' ## Ordination
 
