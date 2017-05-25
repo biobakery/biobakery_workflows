673c5e313c51c819b1b680966081c2b4292b2b18
Lauren McIver <lauren.j.mciver@gmail.com>
2017-05-25 12:42:09 -0600

    Modify the wmgx_wmtx_vis qc template to allow for any number and type of qc filtering databases

--- biobakery_workflows/document_templates/quality_control_paired_dna_rna.template.py
+++ biobakery_workflows/document_templates/quality_control_paired_dna_rna.template.py
@@ -15,39 +15,15 @@ vars = document.get_vars()
 pdf_format = True if vars["format"] == "pdf" else False
 
 # read in the DNA samples
-columns, dna_samples, dna_paired_data = document.read_table(vars["dna_read_counts"], only_data_columns=(0,2,6))
-dna_paired_columns = ["Raw","Trim","hg38"]
-
-columns, dna_samples, dna_orphan_data = document.read_table(vars["dna_read_counts"], only_data_columns=(4,5,8,9))
-dna_orphan_columns = ["Trim orphan1", "Trim orphan2", "hg38 orphan1", "hg38 orphan2"]
+(dna_paired_columns, dna_orphan_columns), dna_samples, (dna_paired_data, dna_orphan_data) = visualizations.qc_read_counts(document, vars["dna_read_counts"])
 
 # read in the RNA samples
-columns, rna_samples, rna_paired_data = document.read_table(vars["rna_read_counts"], only_data_columns=(0,2,6,14))
-rna_paired_columns = ["Raw","Trim","hg38","hg38 mRNA"]
-
-columns, rna_samples, rna_orphan_data = document.read_table(vars["rna_read_counts"], only_data_columns=(4,5,10,11,16,17))
-rna_orphan_columns = ["Trim orphan1", "Trim orphan2", "hg38 orphan1", "hg38 orphan2", 
-    "mRNA orphan1", "mRNA orphan2"]
+(rna_paired_columns, rna_orphan_columns), rna_samples, (rna_paired_data, rna_orphan_data) = visualizations.qc_read_counts(document, vars["rna_read_counts"])
 
 
 #' # Quality Control
 
-#' <%= visualizations.ShotGun.format_caption("qc_intro",total_samples=len(dna_samples)+len(rna_samples),seq_type="paired-end") %>
-#' Reads were first trimmed then filtered using the human genome (hg38) 
-#' to remove reads originating from the host DNA. RNA reads were additionally 
-#' filtered using the human transcriptome (hg38 mRNA) to remove reads originating 
-#' from host gene isoforms.
-
-#' Data is organized by paired and orphan reads. When 
-#' one read in a pair passes a filtering step and the other does not the surviving
-#' read is an orphan. The tables and plots are annotated as follows:
-
-#' * raw: Untouched fastq reads.
-#' * trim: Number of reads remaining after trimming bases with Phred score < 20. If the 
-#' trimmed reads is <70% of original length then it is removed altogether.
-#' * hg38: Number of reads remaining after depleting reads against the human genome (hg38).
-#' * mRNA: Number of reads remaining after depleting reads against the human genome (hg38)
-#' and the human transcriptome (hg38 mRNA). (RNA samples only)
+#' <% visualizations.ShotGun.print_qc_intro_caption(len(dna_samples), dna_paired_columns[2:], paired=True) %>
 
 #+ echo=False
 
@@ -79,7 +55,8 @@ table_message=visualizations.show_table_max_rows(document, dna_orphan_data, dna_
 
 #+ echo=False
 # plot the microbial reads ratios
-dna_microbial_reads, dna_microbial_labels = utilities.microbial_read_proportion(dna_paired_data, dna_orphan_data)
+dna_microbial_reads, dna_microbial_labels = utilities.microbial_read_proportion_multiple_databases(
+    dna_paired_data, dna_paired_columns, dna_orphan_data)
 document.write_table(["# Sample"]+dna_microbial_labels, dna_samples, 
     dna_microbial_reads, files.ShotGunVis.path("microbial_counts",document.data_folder))
 
@@ -131,8 +108,8 @@ table_message=visualizations.show_table_max_rows(document, rna_orphan_data, rna_
 
 #+ echo=False
 # write and plot the microbial reads ratios
-rna_microbial_reads, rna_microbial_labels = utilities.microbial_read_proportion(rna_paired_data,
-    rna_orphan_data,rna=True)
+rna_microbial_reads, rna_microbial_labels = utilities.microbial_read_proportion_multiple_databases(
+    rna_paired_data, rna_paired_columns, rna_orphan_data)
 document.write_table(["# Sample"]+rna_microbial_labels, rna_samples, 
     rna_microbial_reads, files.ShotGunVis.path("rna_microbial_counts",document.data_folder))
 
