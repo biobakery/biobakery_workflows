aa375f4cbf0e787a3814cca01c85803df9e2e06e
Lauren McIver <lauren.j.mciver@gmail.com>
2017-05-30 17:40:23 -0600

    Modify header template so it is also compatible with pweave to pandoc, in addition to pypublish

--- biobakery_workflows/document_templates/header.template.py
+++ biobakery_workflows/document_templates/header.template.py
@@ -1,20 +1,6 @@
-
-#+ echo=False
-import time
-
-from anadama2 import PweaveDocument
-
-document=PweaveDocument()  
-
-# get the variables for this document generation task
-vars = document.get_vars()
-
-# determine the document format
-pdf_format = True if vars["format"] == "pdf" else False
-        
-#' % <% print(vars["title"]) %>
-#' <% if vars["project"]: print("% Project: "+vars["project"]) if pdf_format else print("**Project**: "+vars["project"]) %>  
-#' <% print("% Date: "+ time.strftime("%m/%d/%Y")) if pdf_format else print("**Date**: " + time.strftime("%m/%d/%Y")) %>
+#' % <% from anadama2 import PweaveDocument; document=PweaveDocument(); vars = document.get_vars(); print(vars["title"]) %>
+#' % Project: <% print(vars["project"]) %>
+#' % Date: <% import time; print(time.strftime("%m/%d/%Y")) %>
 
 #' # Introduction
 
