c8267a176f04cf7055329898b893256b31948514
Ana Mailyan <ana.mailyan@gmail.com>
2019-04-15 21:19:08 -0400

    Naming it trunc-len-rev

--- biobakery_workflows/workflows/16s.py
+++ biobakery_workflows/workflows/16s.py
@@ -48,7 +48,7 @@ workflow.add_argument("index-identifier", desc="the string to identify the index
 workflow.add_argument("min-pred-qc-score", desc="the min phred quality score to use for demultiplexing", default=2)
 workflow.add_argument("maxee", desc="the maxee value to use for quality control filtering", default=1)
 workflow.add_argument("trunc-len-max", desc="the max length for truncating reads", default=200)
-workflow.add_argument("trunc-len-max2", desc="the max length for truncating reverse reads for dada2", default=200)
+workflow.add_argument("trunc-len-max-rev", desc="the max length for truncating reverse reads for dada2", default=200)
 workflow.add_argument("min-size", desc="the min size to use for clustering", default=2)
 workflow.add_argument("percent-identity", desc="the percent identity to use for alignments", default=0.97)
 workflow.add_argument("bypass-msa", desc="bypass running multiple sequence alignment and tree generation", action="store_true")
@@ -94,7 +94,7 @@ if args.method == "dada2":
     # filter reads and trim
     read_counts_file_path,  filtered_dir = dadatwo.filter_trim(
             workflow, demultiplex_output_folder,
-            args.output, args.maxee, args.trunc_len_max, args.trunc_len_max2, args.pair_identifier, args.threads)
+            args.output, args.maxee, args.trunc_len_max, args.trunc_len_max_rev, args.pair_identifier, args.threads)
     
     # learn error rates
     error_ratesF_path, error_ratesR_path = dadatwo.learn_error(
