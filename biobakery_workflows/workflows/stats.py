f91804fb34e08937d9b918459a4a80eff015f907
ljmciver <lauren.j.mciver@gmail.com>
2019-10-29 17:48:23 -0600

    Add metadata to humann2 barplot function tasks including new task to create input file in workflow

--- biobakery_workflows/workflows/stats.py
+++ biobakery_workflows/workflows/stats.py
@@ -24,6 +24,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 import os
+import sys
+import re
 
 # import the workflow class from anadama2
 from anadama2 import Workflow
@@ -49,6 +51,9 @@ workflow.add_argument("fixed-effects",desc="the fixed effects to apply to the da
 workflow.add_argument("random-effects",desc="the random effects to apply to the data with MaAsLin2", default="")
 workflow.add_argument("format",desc="the format for the report", default="pdf", choices=["pdf","html"])
 workflow.add_argument("top-pathways",desc="the top N significant pathways to plot stratified abundance", default=3)
+workflow.add_argument("metadata-categorical",desc="the categorical features (for the plot stratified pathways)", action="append", default=[])
+workflow.add_argument("metadata-continuous",desc="the continuous features (for the plot stratified pathways)", action="append", default=[])
+workflow.add_argument("metadata-exclude",desc="the features to exclude (for the plot stratified pathways)", action="append", default=[])
 workflow.add_argument("introduction-text",desc="the text to include in the intro of the report",
     default="The data was run through the standard stats workflow.")
 
@@ -101,21 +106,48 @@ for maaslin_input_file, maaslin_heatmap, maaslin_results_table in maaslin_tasks_
             args=os.path.dirname(maaslin_results_table)))
 
 stratified_pathways_plots = []
+stratified_plots_tasks = []
 if pathabundance:
+    # read in the metadata to merge with the data for the barplot script
+    metadata=utilities.read_metadata(args.input_metadata, pathabundance,
+        name_addition="_Abundance", ignore_features=args.metadata_exclude)
+    metadata_labels, metadata=utilities.label_metadata(metadata, categorical=args.metadata_categorical, continuous=args.metadata_continuous)
+    # get all continuous or samples ids and remove (as they are not to be used for the plots)
+    metadata_exclude=args.metadata_exclude+[x for x,y in filter(lambda x: x[1] == "con", metadata_labels.items())]
+    for metadata_row in metadata[1:]:
+        if len(list(set(metadata_row[1:]))) > utilities.MAX_METADATA_CATEGORIES:
+            metadata_exclude+=[metadata_row[0]]
+    metadata_exclude=list(set(metadata_exclude))
+    metadata=utilities.read_metadata(args.input_metadata, pathabundance,
+        name_addition="_Abundance", ignore_features=metadata_exclude)
+    metadata_labels, metadata=utilities.label_metadata(metadata, categorical=args.metadata_categorical, continuous=args.metadata_continuous)
+
+    humann2_barplot_input = utilities.name_files("merged_data_metadata_input.tsv", args.output, subfolder="stratified_pathways", create_folder=True)
+    workflow.add_task(
+        utilities.partial_function(utilities.create_merged_data_file, metadata=metadata),
+        depends=pathabundance,
+        targets=humann2_barplot_input)
+
+    metadata_row_names=[row[0] for row in metadata[1:]]
+    metadata_end=metadata_row_names[-1]
     for i in range(1,args.top_pathways+1):
-        new_pathways_plot=utilities.name_files("stratified_pathways_{}.jpg".format(i), args.output, subfolder="stratified_pathways", create_folder=True)
-        workflow.add_task(
-            utilities.partial_function(utilities.run_humann2_barplot, number=i),
-            depends=[maaslin_tasks_info[1][2],pathabundance],
-            targets=new_pathways_plot)
-        stratified_pathways_plots.append(new_pathways_plot)
+        for metadata_focus in metadata_row_names:
+            if re.match('^[\w-]+$', metadata_focus) is None:
+                sys.exit("ERROR: Please modify metadata names to include only alpha-numeric characters: "+metadata_focus)
+            new_pathways_plot=utilities.name_files("stratified_pathways_{0}_{1}.jpg".format(metadata_focus,i), args.output, subfolder="stratified_pathways")
+            stratified_plots_tasks.append(workflow.add_task(
+                utilities.partial_function(utilities.run_humann2_barplot, number=i, metadata_end=metadata_end, metadata_focus=metadata_focus),
+                depends=[maaslin_tasks_info[1][2],humann2_barplot_input],
+                targets=new_pathways_plot,
+                name="run_humann2_barplot_pathway_{0}_{1}".format(i, metadata_focus)))
+            stratified_pathways_plots.append(new_pathways_plot)
 
 templates=[utilities.get_package_file("header"),utilities.get_package_file("stats")]
 
 # add the document to the workflow
 doc_task=workflow.add_document(
     templates=templates,
-    depends=maaslin_tasks+[taxonomic_profile], 
+    depends=maaslin_tasks+stratified_plots_tasks+[taxonomic_profile], 
     targets=workflow.name_output_files("stats_report."+args.format),
     vars={"title":"Stats Report",
           "project":args.project_name,
