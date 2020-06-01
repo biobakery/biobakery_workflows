d258f118c6f6ad1c8b5ebe40650620a4322efa92
ljmciver <lauren.j.mciver@gmail.com>
2020-06-01 17:08:34 -0400

    Fix format of new line for picrust change

--- biobakery_workflows/tasks/sixteen_s.py
+++ biobakery_workflows/tasks/sixteen_s.py
@@ -732,7 +732,7 @@ def run_picrust2(task, threads, otus=False):
         with open(reformat_input_tsv, "w") as file_handle_write:
             header = file_handle.readline()
             if otus:
-                header="\t".join(header.split("\t")[:-1]+"\n")
+                header="\t".join(header.split("\t")[:-1])+"\n"
             file_handle_write.write(header)
             for line in file_handle:
                 if otus:
