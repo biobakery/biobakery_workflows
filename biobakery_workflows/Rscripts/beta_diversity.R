526a53316625ef626d305af0a164641f09827d80
ljmciver <lauren.j.mciver@gmail.com>
2020-08-13 16:53:13 -0400

    Add checknames to beta diversity script so as to not change the header name if it starts with a number

--- biobakery_workflows/Rscripts/beta_diversity.R
+++ biobakery_workflows/Rscripts/beta_diversity.R
@@ -85,8 +85,8 @@ if (length(positional_args) != 3) {
 # Input variables
 ## assume data file has samples as columns
 ## allow for pounds in header lines
-data <- t(read.table(positional_args[1], header = TRUE, row.names = 1, sep="\t", comment.char = ""))
-metadata <- data.frame(read.table(positional_args[2], header = TRUE, row.names = 1, sep="\t", comment.char = ""))
+data <- t(read.table(positional_args[1], header = TRUE, row.names = 1, sep="\t", comment.char = "", check.names=FALSE))
+metadata <- data.frame(read.table(positional_args[2], header = TRUE, row.names = 1, sep="\t", comment.char = "", check.names=FALSE))
 
 # check for samples as columns or rows
 samples_rows <- intersect(rownames(metadata),rownames(data))
